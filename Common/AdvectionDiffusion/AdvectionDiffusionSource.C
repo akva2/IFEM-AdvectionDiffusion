d4de3974023584c9e1dbb8fdfccf4968d9f5952d
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-05-27 08:42:35 +0200

    added: add index of scalar solution to be used
    needed in boussinesq

--- Common/AdvectionDiffusion/AdvectionDiffusionSource.C
+++ Common/AdvectionDiffusion/AdvectionDiffusionSource.C
@@ -34,8 +34,10 @@ AdvectionDiffusionAnaSolSource (const AnaSol& aSol,
                                 const VecFunc& U,
                                 const FluidProperties& prop,
                                 const RealFunc* rField,
-                                bool stationary) :
-  anaSol(aSol), adVel(U), props(prop), reaction(rField), stat(stationary)
+                                bool stationary,
+                                int idx)
+  : anaSol(aSol), adVel(U), props(prop), reaction(rField),
+    stat(stationary), scalarIdx(idx)
 {
 }
 
@@ -43,7 +45,7 @@ AdvectionDiffusionAnaSolSource (const AnaSol& aSol,
 Real AdvectionDiffusionAnaSolSource::evaluate (const Vec3& X) const
 {
   Real result;
-  const RealFunc& T = *anaSol.getScalarSol();
+  const RealFunc& T = *anaSol.getScalarSol(scalarIdx);
   const Vec3 dT = T.gradient(X);
   const SymmTensor d2T = T.hessian(X);
 
